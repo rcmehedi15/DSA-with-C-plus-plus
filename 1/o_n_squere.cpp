4fbcc0d714fd888d88acb8ba1f5ef432b9096ca4
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-12-03 22:33:38 +0600

    done'

---
+++ 1/o_n_squere.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n; 
+    cin>>n;
+    for(int i=1;i<=n;i++) // o(n*n)
+    {
+        for(int j=1; j<=n;j++)
+        {
+            cout<<"Hello"<<endl;
+        }
+    }
+    return 0;
+}
