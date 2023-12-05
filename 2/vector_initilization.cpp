d81869ca3fbab7482e240785e90a55e15563fb32
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-12-05 06:38:26 +0600

    v 2

--- 2/vector_initilization.cpp
+++ 2/vector_initilization.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 int main()
 {
-    vector<int> v(5,10);
+    // vector <int> v; type -1
+    // vector <int> v(2); type -2
+    // vector <int> v(2,3); type -3
+    // vector<int> v(5,10);
+    vector<int> v2(2,100);
+    vector<int> v(v2);
     for(int i=0; i<v.size();i++)
     {
         cout<<v[i]<<" ";
