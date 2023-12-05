f0a8586a9f8b15383a499bf9a343859946c5df99
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-12-05 07:18:19 +0600

    capacity done

--- 2/vector_capacity.cpp
+++ 2/vector_capacity.cpp
@@ -17,14 +17,16 @@ int main()
     v.push_back(30);
     v.push_back(40);
     v.push_back(50);
-    v.clear();
+    // v.clear();
+    v.resize(2);
+    v.resize(7,55);
     cout<<v.size()<<endl;
-    // for(int i=0; i<v.size(); i++)
-    // {
-    //     cout<<v[i]<<" ";
+    for(int i=0; i<v.size(); i++)
+    {
+        cout<<v[i]<<" ";
 
-    // }
-    cout<<v[0]<<endl;
+    }
+    // cout<<v[0]<<endl;
 
 
     return 0;
