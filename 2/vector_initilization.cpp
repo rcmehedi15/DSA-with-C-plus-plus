d847e797a259fcb4a0cb6b00039675a260e9def5
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-12-05 06:34:56 +0600

    vector

---
+++ 2/vector_initilization.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v(5,10);
+    for(int i=0; i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    cout<<v.size()<<endl;
+    return 0;
+}
