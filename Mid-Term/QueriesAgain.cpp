d35e69468753af12097af8d02b4d6b4ecab3e676
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-12-16 19:48:13 +0600

    queries again done

--- Mid-Term/QueriesAgain.cpp
+++ Mid-Term/QueriesAgain.cpp
@@ -1,46 +1,118 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int Q;
-    cin >> Q;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
 
-    list<int> l;
-    list<int>::iterator it = l.begin();
+void insert_position(Node *&head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        if (temp == NULL)
+        {
+            cout << "Invalid position" << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    newNode->prev = temp;
+    if (temp->next != NULL)
+    {
+        temp->next->prev = newNode;
+    }
+    temp->next = newNode;
+}
 
-    for (int i = 0; i < Q; i++) {
+void insert_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+
+int size(Node *head)
+{
+    Node *temp = head;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+void print_value(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+void print_reverse(Node *v)
+{
+    if (v == NULL)
+        return;
+    print_reverse(v->next);
+    cout << v->val << " ";
+}
+
+int main()
+{
+    Node *head = NULL;
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++)
+    {
         int x, v;
         cin >> x >> v;
-
-        if (x < 0 || x > static_cast<int>(l.size())) {
+        if (x > size(head))
+        {
             cout << "Invalid" << endl;
-            continue;
         }
-
-        if (x == 0) {
-            l.push_front(v);
-            it = l.begin();
-        } else if (x == static_cast<int>(l.size())) {
-            l.push_back(v);
-            it = l.end();
-            it--;
-        } else {
-            advance(it, x);
-            l.insert(it, v);
-            it = l.begin();  // Reset the iterator to the beginning
+        else if (x == 0)
+        {
+            insert_head(head, v);
+            cout << "L -> ";
+            print_value(head);
+            cout << endl;
+            cout << "R -> ";
+            print_reverse(head);
+            cout << endl;
         }
-
-        cout << "L -> ";
-        for (auto itr = l.begin(); itr != l.end(); itr++) {
-            cout << *itr << " ";
+        else
+        {
+            insert_position(head, x, v);
+            cout << "L -> ";
+            print_value(head);
+            cout << endl;
+            cout << "R -> ";
+            print_reverse(head);
+            cout << endl;
         }
-        cout << endl;
-
-        cout << "R -> ";
-        for (auto ritr = l.rbegin(); ritr != l.rend(); ritr++) {
-            cout << *ritr << " ";
-        }
-        cout << endl;
     }
 
     return 0;
