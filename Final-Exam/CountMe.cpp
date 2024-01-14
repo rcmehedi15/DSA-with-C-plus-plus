a688d9c6bc2ec21beddf2f59f3b19ec68148761d
rcmehedi15 <mdmehedihasan20188@gmail.com>
2024-01-14 19:58:50 +0600

    count me

--- Final-Exam/CountMe.cpp
+++ Final-Exam/CountMe.cpp
@@ -5,35 +5,31 @@ int main()
 {
     int n;
     cin >> n;
+    cin.ignore(); // consume the newline character after reading n
+
     for (int i = 0; i < n; i++)
     {
-        cin.ignore();
         string line;
         getline(cin, line);
+
         string word;
         stringstream ss(line);
         map<string, int> mp;
+
+        string ans; // Updated: to store the word with maximum frequency
+        int mx = 0; // Updated: to store the maximum frequency
+
         while (ss >> word)
         {
             mp[word]++;
-            cout << word << ", ";
-        }
-        int max = 0;
-        string name;
-        pair<string, int> p;
-        for (auto it : mp)
-        {
-            if (it.second > max)
+            if (mp[word] > mx)
             {
-                max = it.second;
-                name = it.first;
-                p.first = it.first;
-                p.second = it.second;
+                mx = mp[word];
+                ans = word;
             }
         }
-       
-        cout << p.first << " " << p.second << endl;
-        cout << name << " " << max << endl;
+
+        cout << ans << " " << mx << endl;
     }
 
     return 0;
