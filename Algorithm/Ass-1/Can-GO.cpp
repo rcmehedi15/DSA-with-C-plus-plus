d17e55f074a7bfe27ec78fc080a508560149e6be
rcmehedi15 <mdmehedihasan20188@gmail.com>
2024-01-29 22:03:07 +0600

    some change

--- Algorithm/Ass-1/Can-GO.cpp
+++ Algorithm/Ass-1/Can-GO.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
 
 using namespace std;
 
-// Function to perform Depth-First Search
+// Function Depth-First Search
 bool dfs(vector<vector<char>>& building, int x, int y, int targetX, int targetY, int height, int width) {
     // Check if the current position is out of bounds or a wall
     if (x < 0 || x >= height || y < 0 || y >= width || building[x][y] == '#') {
         return false;
     }
 
-    // Mark the current cell as visited
     building[x][y] = '#';
 
-    // If the target room is reached, return true
+ 
     if (x == targetX && y == targetY) {
         return true;
     }
@@ -25,9 +24,7 @@ bool dfs(vector<vector<char>>& building, int x, int y, int targetX, int targetY,
            dfs(building, x, y - 1, targetX, targetY, height, width);
 }
 
-// Function to check if you can go from room A to room B
 bool canGo(vector<vector<char>>& building, int height, int width, int startX, int startY, int targetX, int targetY) {
-    // Perform DFS to check connectivity between rooms
     return dfs(building, startX, startY, targetX, targetY, height, width);
 }
 
@@ -58,7 +55,6 @@ int main() {
         }
     }
 
-    // Check if you can go from room A to room B
     if (canGo(building, height, width, startX, startY, targetX, targetY)) {
         cout << "YES" << endl;
     } else {
