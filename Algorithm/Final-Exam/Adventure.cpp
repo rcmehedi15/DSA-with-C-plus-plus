2b18404dad204ab2c30455a69b66f288c87e7208
rcmehedi15 <mdmehedihasan20188@gmail.com>
2024-02-26 12:47:04 +0600

    adventure problem solve done

---
+++ Algorithm/Final-Exam/Adventure.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int knapsack(int W, int wt[], int val[], int n) {
+    int dp[n + 1][W + 1];
+    
+    for (int i = 0; i <= n; ++i) {
+        for (int w = 0; w <= W; ++w) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+    
+    return dp[n][W];
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, W;
+        cin >> n >> W;
+
+        int wt[n], val[n];
+
+        for (int i = 0; i < n; ++i)
+            cin >> wt[i];
+
+        for (int i = 0; i < n; ++i)
+            cin >> val[i];
+
+        int result = knapsack(W, wt, val, n);
+        cout << result << endl;
+    }
+
+    return 0;
+}
